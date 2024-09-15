9ecc5026a37268f918b7173040dd89b0c46bc062
ming <13318718863@163.com>
2024-09-15 12:37:24 +0800

    测试图片传入

--- src/main/java/com/must/atm/mustatm/Controller/VerificationPageController.java
+++ src/main/java/com/must/atm/mustatm/Controller/VerificationPageController.java
@@ -10,6 +10,10 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -44,8 +48,8 @@ public class VerificationPageController {
 
         // 计算图片位置
         anchorPane.heightProperty().addListener((obs, oldVal, newVal) -> {
-            double topAnchor = (newVal.doubleValue() )*0.25;
-            AnchorPane.setTopAnchor(verificationWindowView, topAnchor);
+            double picHight = (newVal.doubleValue() )*0.25;
+            AnchorPane.setTopAnchor(verificationWindowView, picHight);
         });
 
         anchorPane.widthProperty().addListener((obs, oldVal, newVal) -> {
@@ -64,6 +68,64 @@ public class VerificationPageController {
         });
 
 
+        // 创建 Text 对象
+        Text text = new Text("Styled Text");
+
+        // 设置字体样式（字体，字体加粗，字体斜体，字体大小）
+        text.setFont(Font.font("Verdana", FontWeight.BOLD, FontPosture.ITALIC, 30));
+
+        // 设置字体颜色
+        text.setFill(Color.BLUE);
+
+        //计算字体位置
+        double x = 0.0;
+        double y = 0.0;
+
+        //test
+//        for (int i = 1; i < 100; i++)
+//        {
+//            // 创建must对象
+//            String mustPath = "pictureOfMust.png";
+//            Image must = new Image(mustPath);
+//
+//            // 创建ImageView并设置图片
+//            ImageView mustView = new ImageView(must);
+//            anchorPane.getChildren().add(mustView);
+//
+//            // 设置must大小
+//            mustView.setFitWidth(1000);
+//            mustView.setFitHeight(1000);
+//            mustView.setPreserveRatio(true);
+//
+//            // 设置must位置
+//            AnchorPane.setTopAnchor(mustView, x);
+//            AnchorPane.setLeftAnchor(mustView, y);
+//
+//            //计算must大小
+//            anchorPane.widthProperty().addListener((obs, oldVal, newVal) ->
+//            {
+//                double topAnchor = newVal.doubleValue() * 0.45;
+//                mustView.setFitWidth(topAnchor);
+//            });
+//            anchorPane.heightProperty().addListener((obs, oldVal, newVal) ->
+//            {
+//                double topAnchor = newVal.doubleValue() * 0.45;
+//                mustView.setFitHeight(topAnchor);
+//            });
+//            x += 0.1;
+//            y += 0.1;
+//            try {
+//                Thread.sleep(100);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+
+
+
+
+
         //创建bar
         var bar1 = new ProgressBar(0);
         bar1.setPrefWidth(200);
@@ -76,7 +138,7 @@ public class VerificationPageController {
                 Task <Double> task = new Task<Double>() {
                     @Override
                     protected Double call() throws Exception {
-                        return  i=i+0.01;
+                        return  i=i+0.1;
 
                     }
 
@@ -99,13 +161,13 @@ public class VerificationPageController {
         //bar位置
         anchorPane.heightProperty().addListener((obs, oldVal, newVal) -> {
             double topAnchor = (newVal.doubleValue()*0.5 );
-            bar1.setLayoutX(topAnchor);
+            bar1.setLayoutY(topAnchor);
 //            bar1.setPrefWidth(topAnchor);
         });
 
         anchorPane.widthProperty().addListener((obs, oldVal, newVal) -> {
-            double leftAnchor = (newVal.doubleValue() *0.25);
-            bar1.setLayoutY(leftAnchor);
+            double leftAnchor = (newVal.doubleValue() *0.35);
+            bar1.setLayoutX(leftAnchor);
         });
         anchorPane.getChildren().add(bar1);
 
