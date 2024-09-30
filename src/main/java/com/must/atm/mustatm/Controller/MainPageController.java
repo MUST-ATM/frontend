0fe8746c46736ed18cb70c031a60e64192687793
ming <13318718863@163.com>
2024-09-30 14:05:01 +0800

    阶段性陈果

--- src/main/java/com/must/atm/mustatm/Controller/MainPageController.java
+++ src/main/java/com/must/atm/mustatm/Controller/MainPageController.java
@@ -1,10 +1,5 @@
-
 package com.must.atm.mustatm.Controller;
 
-import com.must.atm.mustatm.Template.BaseRectangle;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -15,9 +10,6 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * MainPageController is a class which can generate main page
@@ -37,9 +29,18 @@ public class MainPageController {
         //instantiate a controller
         Controller controller = new Controller();
 
-
         //creat topPane
         HBox topPane = new HBox();
+        basePane.setTop(topPane);
+
+        //create leftPane
+        AnchorPane leftPane = new AnchorPane();
+        basePane.setLeft(leftPane);
+
+        //create rightPane
+        AnchorPane rightPane = new AnchorPane();
+        basePane.setRight(rightPane);
+
 
         // instantiate a topBar
         Image topBar = new Image("topBar.png");
@@ -48,30 +49,15 @@ public class MainPageController {
         topBarView.setPreserveRatio(true);
         topBarView.fitWidthProperty().bind(basePane.widthProperty());
         topPane.getChildren().add(topBarView);
-        basePane.setTop(topPane);
 
-
-
-
-
-        //create rightPane
-        AnchorPane rightPane = new AnchorPane();
-
-        rightPane.setStyle("-fx-background-color:#008000 ;");
-
+        //set rectangle
         Rectangle rectangle = new Rectangle();
         rectangle.setFill(Color.rgb(5,80,174));
-
         rectangle.setHeight(rightPane.getHeight()*0.15);
         rectangle.setWidth(rightPane.getWidth()*0.7);
-
+        rightPane.setBottomAnchor(rectangle, 0.0);
+        rightPane.setRightAnchor(rectangle, 0.0);
         rightPane.getChildren().add(rectangle);
-//        rectangle.setHeight(rightPane.getHeight()/6.0);
-//        rectangle.setWidth(rightPane.getWidth()/3.0);
-//        rightPane.setTopAnchor(rectangle, rightPane.getHeight() / 6.0);
-//        rightPane.setBottomAnchor(rectangle, 0.0);
-//        rightPane.setLeftAnchor(rectangle, rightPane.getWidth() / 3.0);
-//        rightPane.setRightAnchor(rectangle, 0.0);
 
         // instantiate an image which is the picture of MUST
         Image must = new Image("pictureOfMust.png");
@@ -79,75 +65,6 @@ public class MainPageController {
         ImageView mustView = new ImageView(must);
         mustView.setPreserveRatio(true);
         rightPane.getChildren().add(mustView);
-        // set the default size of image
-        mustView.setFitWidth(100);
-        mustView.setFitHeight(100);
-        mustView.setPreserveRatio(true);
-        // set the position of the image
-        basePane.heightProperty().addListener((obs, oldVal, newVal) -> {
-            double topAnchor = newVal.doubleValue() * 0.1;
-            AnchorPane.setTopAnchor(mustView, topAnchor);
-        });
-
-
-
-        rightPane.setBottomAnchor(rectangle, 0.0);
-        rightPane.setRightAnchor(rectangle, 0.0);
-//        primaryStage.heightProperty().addListener(new ChangeListener<Number>() {
-//            @Override
-//            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-//
-//                rightPane.setPrefHeight(basePane.getHeight());
-//
-//            rightPane.setTopAnchor(mustView, rightPane.getHeight()*0.1);
-//            rightPane.setBottomAnchor(mustView,rightPane.getHeight()*0.4);
-//
-//                rectangle.setHeight(rightPane.getHeight()*0.15);
-//                mustView.setFitHeight(rightPane.getHeight()*0.5);
-//            }
-//        });
-
-
-//        primaryStage.widthProperty().addListener(new ChangeListener<Number>() {
-//            @Override
-//            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-//
-//                rightPane.setPrefWidth(basePane.getWidth()*0.7);
-//
-//                rightPane.setRightAnchor(mustView, rightPane.getWidth()*0.2);
-//                rightPane.setLeftAnchor(mustView, rightPane.getWidth()*0.1);
-//
-//                rectangle.setWidth(rightPane.getWidth()*0.8);
-//                mustView.setFitWidth(rightPane.getWidth()*0.7);
-//            }
-//        });
-
-        basePane.setRight(rightPane);
-
-
-        //create buttonPane
-        HBox bottomPane = new HBox();
-
-        bottomPane.setStyle("-fx-background-color:linear-gradient(to bottom,#FFFF00,#FFFF00) ;");
-
-//        // add a buttonbar
-//        // input the data of bar in the list
-//        ArrayList<Integer>recColor = new ArrayList<>(Arrays.asList(5,80,174));
-//        ArrayList<Double>recPosition = new ArrayList<>(Arrays.asList(0.9,0.6,0.15,4.0));
-//        //instantiate a BaseRectangle which can encapsulate the data
-//        BaseRectangle rec =new BaseRectangle(0.0,0.0,recColor,recPosition);
-//        Rectangle rectangle = controller.createRectangle(rec);
-//        controller.setRectangle(basePane,rectangle,rec);
-//        basePane.getChildren().add(rectangle);
-//        basePane.setBottom(bottomPane);
-
-
-
-        //create leftPane
-        AnchorPane leftPane = new AnchorPane();
-        leftPane.setMinHeight(110);
-        leftPane.setStyle("-fx-background-color:linear-gradient(to bottom,#0000FF,#0000FF) ;");
-
 
         // create button
         var normalBtn = new Button("SERVICE");
@@ -157,13 +74,9 @@ public class MainPageController {
         normalBtn.setPrefSize(300, 60);
         normalBtn.setFont(Font.font("Inter", FontWeight.BOLD, FontPosture.REGULAR, 20));
         normalBtn.setStyle("-fx-text-fill: #033D8B;");
-//        leftPane.setBottomAnchor(normalBtn, 50.0);
-//        leftPane.setLeftAnchor(normalBtn, 50.0);
         leftPane.getChildren().add(normalBtn);
-        basePane.setLeft(leftPane);
 
-
-        //set the size of image
+        //set listener
         basePane.widthProperty().addListener((obs, oldVal, newVal) -> {
             double topAnchor = newVal.doubleValue()*0.45;
             rectangle.setWidth(basePane.getWidth()*0.5);
@@ -171,50 +84,13 @@ public class MainPageController {
             leftPane.setLeftAnchor(normalBtn, leftPane.getWidth()*0.10);
             mustView.setFitWidth (topAnchor);
         });
-
-
         basePane.heightProperty().addListener((obs, oldVal, newVal) -> {
             double topAnchor = newVal.doubleValue()*0.45;
             rectangle.setHeight(basePane.getHeight()*0.1);
             leftPane.setBottomAnchor(normalBtn, leftPane.getHeight()*0.15);
-            mustView.setFitHeight (topAnchor);
+            AnchorPane.setTopAnchor(mustView, topAnchor*0.1);
         });
-
-//        primaryStage.heightProperty().addListener(new ChangeListener<Number>() {
-//            @Override
-//            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-//
-//
-//                rightPane.setPrefHeight(basePane.getHeight());
-//                leftPane.setPrefHeight(basePane.getHeight());
-//
-//                rightPane.setTopAnchor(mustView, rightPane.getHeight()*0.1);
-//                rightPane.setBottomAnchor(mustView,rightPane.getHeight()*0.35);
-//                leftPane.setBottomAnchor(normalBtn, leftPane.getHeight()*0.15);
-//
-//                rectangle.setHeight(rightPane.getHeight()*0.15);
-//                mustView.setFitHeight(basePane.getHeight()*0.55);
-//            }
-//        });
-//
-//
-//        primaryStage.widthProperty().addListener(new ChangeListener<Number>() {
-//            @Override
-//            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-//
-//                rightPane.setPrefWidth(basePane.getWidth()*0.7);
-//                leftPane.setPrefWidth(basePane.getWidth()*0.25);
-//
-//                rightPane.setRightAnchor(mustView, rightPane.getWidth()*0.2);
-//                rightPane.setLeftAnchor(mustView, rightPane.getWidth()*0.1);
-//                leftPane.setLeftAnchor(normalBtn, leftPane.getWidth()*0.10);
-//
-//                rectangle.setWidth(rightPane.getWidth()*0.7);
-//                mustView.setFitWidth(basePane.getWidth()*0.7);
-//
-//            }
-//        });
-
+        
         return basePane;
     }
 
