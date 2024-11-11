adafe6b0e1214b0f7580fb11f3b8e318b3a4b167
小火 <49596040+xiaohuo-desu@users.noreply.github.com>
2024-11-11 20:07:53 +0800

    Completed the Action service.

--- src/main/java/com/must/atm/mustatm/Service/NetworkServiceImpl.java
+++ src/main/java/com/must/atm/mustatm/Service/NetworkServiceImpl.java
@@ -1,9 +1,7 @@
 package com.must.atm.mustatm.Service;
 
-import java.io.IOException;
 import java.net.Authenticator;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -15,6 +13,13 @@ import java.time.Duration;
 public class NetworkServiceImpl implements NetworkService
 {
     String server = Init.getProperties().getProperty("server");
+    /**
+     * Send image to server
+     * @param api api address
+     * @param stream image stream
+     * @return rawFaceId
+     * @throws Exception exception
+     */
     @Override
     public HttpResponse<String> sendImage(String api, byte[] stream) throws Exception
     {
@@ -37,7 +42,12 @@ public class NetworkServiceImpl implements NetworkService
             throw new Exception(e.getMessage());
         }
     }
-
+    /**
+     * Send request to server
+     * @param api api address
+     * @return response from server
+     * @throws Exception exception
+     */
     @Override
     public HttpResponse<String> request(String api) throws Exception
     {
@@ -52,6 +62,36 @@ public class NetworkServiceImpl implements NetworkService
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(new URI(server + api))
                     .GET()
+                    .build();
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.getMessage());
+        }
+    }
+    /**
+     * Send json to server
+     * @param api api address
+     * @param json json string
+     * @return response from server
+     * @throws Exception exception
+     */
+    @Override
+    public HttpResponse<String> sendJson(String api, String json) throws Exception
+    {
+        try
+        {
+            HttpClient client = HttpClient.newBuilder()
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .followRedirects(HttpClient.Redirect.NORMAL)
+                    .connectTimeout(Duration.ofSeconds(20))
+                    .authenticator(Authenticator.getDefault())
+                    .build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(server + api))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(json))
                     .build();
             return client.send(request, HttpResponse.BodyHandlers.ofString());
         }
