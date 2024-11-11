3046cd891138e2af107e047484b4cdeb9eb9542d
小火 <49596040+xiaohuo-desu@users.noreply.github.com>
2024-11-11 20:07:53 +0800

    Completed the Action service.

--- src/main/java/com/must/atm/mustatm/Service/ActionService.java
+++ src/main/java/com/must/atm/mustatm/Service/ActionService.java
@@ -1,14 +1,11 @@
 package com.must.atm.mustatm.Service;
 
-import com.must.atm.mustatm.Service.cards.cardType;
-
-import java.util.HashMap;
+import com.must.atm.mustatm.Service.Type.cardType;
 
 /**
  * @author bywang
  */
 public interface ActionService
 {
-    int getCardId(HashMap<cardType, Integer> cards);
-    double getBalance(int userId,cardType cardType);
+    void setBalance(int userId,cardType cardType,double amount);
 }
--- src/main/java/com/must/atm/mustatm/Service/NetworkService.java
+++ src/main/java/com/must/atm/mustatm/Service/NetworkService.java
@@ -1,14 +1,14 @@
 package com.must.atm.mustatm.Service;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.http.HttpResponse;
 
 /**
+ * Network service
  * @author bywang
  */
 public interface NetworkService
 {
     HttpResponse<String>sendImage(String api,byte[] stream) throws Exception;
     HttpResponse<String>request(String api) throws Exception;
+    HttpResponse<String> sendJson(String api, String json) throws Exception;
 }
